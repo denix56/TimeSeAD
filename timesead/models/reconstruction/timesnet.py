2e67fee1d75704c582358837964bc25196eb8578
denyssenkin <denisx9.0c@gmail.com>
2025-12-08 15:35:42 +0100

    Perf improvements

--- timesead/models/reconstruction/timesnet.py
+++ timesead/models/reconstruction/timesnet.py
@@ -111,7 +111,7 @@ class TimesNet(BaseModel):
         means = x_enc.mean(1, keepdim=True).detach()
         x_enc = x_enc - means
         stdev = torch.sqrt(
-            torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+            torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + torch.finfo(x_enc.dtype).eps)
         x_enc /= stdev
 
         # embedding
