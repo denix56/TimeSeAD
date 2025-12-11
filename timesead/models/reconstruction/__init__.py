80da19c001156f6ea744388e46e75de8b2258672
denyssenkin <denisx9.0c@gmail.com>
2025-12-11 21:39:01 +0100

    Fix Lstm AD

--- timesead/models/reconstruction/__init__.py
+++ timesead/models/reconstruction/__init__.py
@@ -1,4 +1,4 @@
-from .lstm_ae import LSTMAE, LSTMAEMalhotra2016, LSTMAEMirza2018, LSTMAEAnomalyDetector, LSTMAEAnomalyDetectorV2
+from .lstm_ae import LSTMAE, LSTMAEMalhotra2016, LSTMAEMirza2018, LSTMAEAnomalyDetector
 from .mscred import MSCRED, MSCREDLoss, MSCREDAnomalyDetector, MSCREDAnomalyDetectorOrig, SignatureMatrixTransform
 from .tcn_ae import TCNAE, TCNAEAnomalyDetector
 from .usad import USADModel, BasicAE, USADDecoder1Loss, USADDecoder2Loss, USADAnomalyDetector
